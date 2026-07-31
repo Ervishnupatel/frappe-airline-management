9ec85ce81a238f5707f095b9c24e76b893aec2c8
Vishnu Patel <vishnupatelm4@gmail.com>
2026-07-31 15:39:19 +0530

    Completed Airplane Ticket and Airplane Flight changes

--- sky_app/sky/doctype/airplane_ticket/airplane_ticket.py
+++ sky_app/sky/doctype/airplane_ticket/airplane_ticket.py
@@ -1,7 +1,13 @@
+import frappe
 from frappe.model.document import Document
 
-
 class AirplaneTicket(Document):
+
+    def before_naming(self):
+        if self.flight:
+            flight = frappe.get_doc("Airplane Flight", self.flight)
+            self.source_airport_code = flight.source_airport_code
+            self.destination_airport_code = flight.destination_airport_code
 
     def validate(self):
         unique_items = []
@@ -21,3 +27,7 @@ class AirplaneTicket(Document):
             total += addon.amount or 0
 
         self.total_amount = total
+
+    def on_submit(self):
+        if self.status != "Boarded":
+            frappe.throw("Only boarded tickets can be submitted.")
