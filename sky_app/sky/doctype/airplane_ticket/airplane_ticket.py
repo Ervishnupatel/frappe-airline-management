e2b3a622947dec3b8113212d88a23b333f1e1705
Vishnu Patel <vishnupatelm4@gmail.com>
2026-07-31 14:29:51 +0530

    Calculate total amount including add-ons

--- sky_app/sky/doctype/airplane_ticket/airplane_ticket.py
+++ sky_app/sky/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,15 @@
-# Copyright (c) 2026, xzy and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+frappe.msgprint("before_save is running")
 
 class AirplaneTicket(Document):
-	pass
+
+    def before_save(self):
+        frappe.msgprint("before_save is running")
+
+        total = self.flight_price or 0
+
+        for addon in self.add_ons:
+            total += addon.amount or 0
+
+        self.total_amount = total
